154d213f47c895ebcd5a07faa78215a3343a87be
Abhimanyu <mail2abhimanyu.gupta@gmail.com>
2023-07-11 18:21:28 +0530

    setup db connection, routes and controllers

--- app.js
+++ app.js
@@ -1,27 +1,25 @@
 require('dotenv').config();
 require('express-async-errors');
 const express = require('express');
-const app = express();
-
-// error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
+const authRouter = require('./routes/auth');
+const jobsRouter = require('./routes/jobs');
+const connectDB = require('./db/connect');
 
-app.use(express.json());
-// extra packages
+const app = express();
+const port = process.env.PORT || 3000;
 
-// routes
-app.get('/', (req, res) => {
-  res.send('jobs api');
-});
-
+// middlewares
+app.use(express.json()); // for reading req.body
+app.use('/api/v1/auth', authRouter); // routers
+app.use('/api/v1/jobs', jobsRouter); // routers
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-const port = process.env.PORT || 3000;
-
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
