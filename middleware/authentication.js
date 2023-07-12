c3510eb7bce45246d3e8b237198b701ffbf6b10e
Abhimanyu <mail2abhimanyu.gupta@gmail.com>
2023-07-12 09:43:37 +0530

    fixed typo

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -4,7 +4,7 @@ const { UnauthenticatedError } = require('../errors');
 const authMiddleware = async (req, res, next) => {
   const authHeader = req.headers.authorization;
   if (!authHeader || !authHeader.startsWith('Bearer ')) {
-    throw new UnauthenticatedError('UnAuthorization');
+    throw new UnauthenticatedError('Unauthorized');
   }
   const token = authHeader.split(' ')[1];
   try {
@@ -12,7 +12,7 @@ const authMiddleware = async (req, res, next) => {
     req.user = { userId: payload.userId };
     next();
   } catch (error) {
-    throw new UnauthenticatedError('UnAuthorization');
+    throw new UnauthenticatedError('Unauthorized');
   }
 };
 
